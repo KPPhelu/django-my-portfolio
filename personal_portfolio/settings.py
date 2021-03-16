0b62c33afd0843508fe0e3e5f485fde443ca02a1
KPPhelu <krishna.phelu@gmail.com>
2021-03-16 15:42:54 +0545

    new local setting added

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4=ob^l-ea)en!zdbq)7ypmoh3e7s5&xu@izg0h1z%fy=pqo562'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kpphelu.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file. You must be on production.')
